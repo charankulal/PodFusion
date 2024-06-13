df023d717213512334aa74d85902472a700ae812
Charan <charankulal0241@gmail.com>
2024-06-13 09:18:13 +0530

    "Updated ConvexProvider to ConvexClerkProvider, removed ConvexProvider file, and added @clerk/nextjs dependency"

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://renewed-stud-71.clerk.accounts.dev
+
+const authConfig ={
+    providers: [
+      {
+        domain: "https://renewed-stud-71.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
