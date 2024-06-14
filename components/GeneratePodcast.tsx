bc799810c55f590366d58088486d3a51e7555caa
Charan <charankulal0241@gmail.com>
2024-06-14 18:43:06 +0530

    Designed the simple form for creating the podcast for the user.

---
+++ components/GeneratePodcast.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GeneratePodcast = () => {
+  return (
+    <div>
+      Generate Podcast
+    </div>
+  )
+}
+
+export default GeneratePodcast
---
+++ components/GenerateThumbnail.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenerateThumbnail = () => {
+  return (
+    <div>
+      Generate Thumbnail
+    </div>
+  )
+}
+
+export default GenerateThumbnail
